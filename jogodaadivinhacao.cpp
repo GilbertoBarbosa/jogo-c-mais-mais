57a6f4d431073e5139f53625b5f7a82d8d3b324f
GilbertoBarbosa <gisantos555@gmmail.com>
2022-08-27 10:51:09 -0300

    Capítulo 2

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -7,10 +7,38 @@ int main (){
     cout << "* Bem - vindos ao jogo da adivinhação *" << endl;
     cout << "***************************************" << endl;
 
-    int numero_secreto = 42;
+    const int NUMERO_SECRETO = 42;
 
-    cout << "O número secreto é " << numero_secreto << ". Não conte para ninguém!" << endl;
+    bool nao_acertou = true;
+    int tentativas = 0;
 
+    while (nao_acertou) {
+
+        tentativas++;
+        int chute;
+
+        cout << "Tentativa: " << tentativas << endl; 
+        cout << "Qual seu chute? ";
+        cin >> chute;
+        cout << "O valor do seu chute é: " << chute << endl;
+
+        bool acertou = chute == NUMERO_SECRETO;
+        bool maior = chute > NUMERO_SECRETO;
+
+        if (acertou) {
+            cout << "Parabéns, você acertou o número secreto! " << endl;
+            nao_acertou = false;
+        } 
+        else if (maior) {
+            cout << "O número que você chutou é maior que o número secreto!" << endl;
+        }
+        else {
+            cout << "O número que você chutou é menor que o número secreto!" << endl;
+        }
+    }
+
+    cout << "Fim de Jogo!" << endl;
+    cout << "Você acertou o número secreto em " << tentativas  << " tentativas! " << endl;
 }
 
 
